a86f26fa7c033806d8980b7792c978c2b6e42fea
Adria <adria.jorda.m@gmail.com>
2025-06-10 08:49:45 +0200

    [add] timers  con explicacion

---
+++ timers.js
@@ -0,0 +1,10 @@
+// setInterval ejecuta  un codigo cada intervalo de tiempo
+// setTimeout ejecuta un codigo despues de cierto tiempo 
+
+// setInterval(() => {
+//     console.log("Hello world")
+// }, 2000) //ejecuta cada dos segundos
+
+setTimeout(() => {
+    console.log("Bye world")
+}, 2000)
