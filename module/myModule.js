58fb3fddf34ec42cdac3e7195c9f8aed97f54ca3
Adria <adria.jorda.m@gmail.com>
2025-06-10 09:45:42 +0200

    [add] exportar modulos

---
+++ module/myModule.js
@@ -0,0 +1,8 @@
+const myWebAdress = "myweb.com"
+const myNumber = 30
+const names = ['joe', 'fatima', 'lucy']
+const group = "mygroup.com"
+
+module.exports = myWebAdress // decidimos que exportamos y que no
+
+console.log(module) // se pueden ver parametros, entre ellos destacar exports que lo puedes parametrizar con la sintaxis de arriba
