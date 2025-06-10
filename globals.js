e94802cba665f8adeb2788d460b365d9a8a94313
Adria <adria.jorda.m@gmail.com>
2025-06-10 08:44:00 +0200

    [add] globales

---
+++ globals.js
@@ -0,0 +1,8 @@
+// console.log(__dirname) // se esta volviendo la ubicacion del archivo
+// console.log(__filename) // se esta volviendo la ubicacion del archivo y el archivo
+
+// console.log(module) //informacion en json
+
+// console.log(require)
+
+console.log(process) //informacion del sistema
