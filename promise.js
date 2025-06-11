5dcd027dc3ea6ddf6355668cd1697d279f817e86
Adria <adria.jorda.m@gmail.com>
2025-06-11 12:02:40 +0200

    [add] promises y async await

--- promise.js
+++ promise.js
@@ -1,5 +1,6 @@
-const {readFile} = require('fs')
-
+// const {readFile} = require('fs')
+const {readFile} = require('fs/promises') // ya son promesas
+const {promisify} = require('util')
 
 function gettext(pathFile) {
     // es nativo el objeto
@@ -15,6 +16,42 @@ function gettext(pathFile) {
 }
 
 // recordar que es una promesa
-gettext('./data/text.txt')
-    .then((result) => console.log(result))
-    .catch((error) => console.log(error))
+// gettext('./data/text.txt')
+//     .then((result) => console.log(result))
+//     .catch((error) => console.log(error))
+
+async function readText() {
+    try {
+        const result = await gettext('./data/text.txt')
+        console.log(result)
+    }
+    catch(error) {
+        console.log(error)
+    }
+}
+
+
+// promisify que es mas optimo que usar async await
+// const readfilePromise = promisify(readFile)
+// readText() 
+// async function readText() {
+//     try {
+//         const result = await readfilePromise('./data/text2.txt', 'utf-8')
+//         console.log(result)
+//     }
+//     catch(error) {
+//         console.log(error)
+//     }
+// }
+
+ 
+async function read() {
+    try {
+        const result = await readFile('./data/text2.txt', 'utf-8')
+        console.log(result)
+    }
+    catch(error) {
+        console.log(error)
+    }
+}
+read()
